7739bff89a036e37b8807767d01ac30c066f1d54
muhammadjon8 <90293805+muhammadjon8@users.noreply.github.com>
2024-10-03 20:17:06 +0500

    task1 commpleted

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+//
