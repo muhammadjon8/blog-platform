1f5351831e2e753481528a63853e97e3e24142ed
muhammadjon8 <90293805+muhammadjon8@users.noreply.github.com>
2024-10-26 18:09:56 +0500

    #

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -93,7 +93,7 @@ export const getBlogById = async (req: any, res: any) => {
     } else {
       return res.status(400).json({ message: (error as any).message })
     }
-  }
+  } 
 }
 export const getBlogsBySearchParams = async (req: any, res: any) => {
   const page = req.query.page || 1
